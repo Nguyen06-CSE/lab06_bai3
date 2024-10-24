165a8f39a4ef71b1c760ec0b20aeed55bc242bbf
Nguyen06-CSE <nguyen.cse06@gmail.com>
2024-10-24 08:58:39 +0700

    chua co tinh nang tinh hang

--- Menu.h
+++ Menu.h
@@ -3,7 +3,7 @@
 //Khai bao nguyen mau
 void XuatMenu();
 int ChonMenu(int soMenu);
-void XuLyMenu(int chon, MaTran a, unsigned int& n);
+void XuLyMenu(int chon, MaTran a, MaTran b, unsigned int& n);
 void ChayChuongTrinh();
 
 
@@ -13,12 +13,13 @@ void XuatMenu()
 	cout << "\n0. Thoat chuong trinh";
 	cout << "\n1. nhap ma tran vuong cap n ";
 	cout << "\n2. Xuat ma tran";
-	cout << "\n3. tim ma tran chuyen vi cua ma tran M cho truoc";
-    cout << "\n4. tinh hang cua ma tran ";
-    cout << "\n5. cong hai ma tran cung cap";
-    cout << "\n6. tru hai ma tran cung cap";
-    cout << "\n7. nhan ma tran voi mot so";
-    cout << "\n8. nhan hai ma tran";
+	cout << "\n3. tao mang ngau nhien";
+	cout << "\n4. tim ma tran chuyen vi cua ma tran M cho truoc";
+    cout << "\n5. tinh hang cua ma tran ";
+    cout << "\n6. cong hai ma tran cung cap";
+    cout << "\n7. tru hai ma tran cung cap";
+    cout << "\n8. nhan ma tran voi mot so";
+    cout << "\n9. nhan hai ma tran";
 	
 	
 }
@@ -42,7 +43,7 @@ int ChonMenu(int soMenu)
 
 
 
-void XuLyMenu(int chon, MaTran a, unsigned int& n)
+void XuLyMenu(int chon, MaTran a, MaTran b, unsigned int& n)
 {
     
     
@@ -75,8 +76,21 @@ void XuLyMenu(int chon, MaTran a, unsigned int& n)
         cout << endl;
 
 		break;
-    
     case 3:
+
+        int minVal, maxVal;
+		cout << "nhap VUNG SO ban muon (max min): ";
+		cin >> maxVal >> minVal;
+		cout << "nhap cap cua ma tran: ";
+        cin >> n;
+		taoMaTranNgauNhien(b ,n, maxVal, minVal);
+        cout << "mang duoc tao ngau nhien la: \n";
+		xuatMaTran(b, n);
+        
+		
+
+		break;
+    case 4:
 
         
         cout << "ma tran truoc khi duoc hoan vi la: \n";
@@ -84,8 +98,48 @@ void XuLyMenu(int chon, MaTran a, unsigned int& n)
         hoanViMaTran(a, n);
         cout << "ma tran sau khi duoc hoan vi la: \n";
         xuatMaTran(a, n);
-        
-		
+    case 5:
+		break;
+	case 6:
+		cout << "hai ma tran ban vua tao la: \n";
+		xuatMaTran(a, n);
+		cout << endl;
+		xuatMaTran(b, n);
+		cout << endl;
+		cout << "6. cong hai ma tran cung cap\n";
+		congHaiMaTranCungCap(a, b, n);
+		xuatMaTran(a, n);
+
+		break;
+	case 7:
+		cout << "7. tru hai ma tran cung cap\n";
+		cout << "hai ma tran ban vua tao la: \n";
+		xuatMaTran(a, n);
+		cout << endl;
+		xuatMaTran(b, n);
+		cout << endl;
+		truHaiMaTranCungCap(a, b, n);
+		cout << "mang sau ki tru la: \n";
+		xuatMaTran(a, n);
+
+		break;
+	case 8:
+		cout << "8. nhan hai ma tran cung cap \n";
+		int soNhan;
+		cout << "nhap so nhan ma ban muon: ";
+		cin >> soNhan;
+		nhanMaTranVoiMotSo(a, n, soNhan);
+		xuatMaTran(a, n);
+	case 9:
+		cout << "9. nhan hai ma tran cung cap\n";
+		cout << "hai ma tran ban vua tao la: \n";
+		xuatMaTran(a, n);
+		cout << endl;
+		xuatMaTran(b, n);
+		cout << endl;
+		nhanHaiMaTranCungCap(a, b, n);
+		cout << "mang sau ki nhan la: \n";
+		xuatMaTran(a, n);
 
 		break;
     
@@ -101,16 +155,16 @@ void XuLyMenu(int chon, MaTran a, unsigned int& n)
 void ChayChuongTrinh()
 {
     
-	int soMenu = 8, chon;
+	int soMenu = 9, chon;
     unsigned int n;
-    MaTran a;
+    MaTran a, b;
     
 	do
 	{
 		system("cls");
 		chon = ChonMenu(soMenu);
 
-		XuLyMenu(chon, a, n);
+		XuLyMenu(chon, a, b, n);
 
 	} while (chon != 0);
 }
--- ThuVien.h
+++ ThuVien.h
@@ -8,6 +8,12 @@ void nhapMaTran(MaTran a, unsigned int& n);
 void xuatMaTran(MaTran a, unsigned int& n);
 void hoanVi(int& a, int& b);
 void hoanViMaTran(MaTran a, int n);
+void taoMaTranNgauNhien(MaTran a, int n, int maxVal, int minVal);
+void taoMaTranNgauNhien(MaTran b, int n, int maxVal, int minVal);
+void congHaiMaTranCungCap(MaTran a, MaTran b, int n);
+void truHaiMaTranCungCap(MaTran a, MaTran b, int n);
+void nhanMaTranVoiMotSo(MaTran a, int n, int soNhan);
+void nhanHaiMaTranCungCap(MaTran a, MaTran b, int n);
 
 
 void nhapMaTran(MaTran a, unsigned int& n){
@@ -89,3 +95,52 @@ void hoanViMaTran(MaTran a, int n) {
         --right;
     }
 }
+
+
+    
+void taoMaTranNgauNhien(MaTran b, int n, int maxVal, int minVal){
+    srand(time(0));
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            b[i][j] = rand() % (maxVal - minVal + 1) + minVal;
+        //     if(rand() % 2 == 0){
+        //         b[i][j] = - b[i][j];
+        //     }
+        }
+    }
+}
+
+
+void congHaiMaTranCungCap(MaTran a, MaTran b, int n){
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            a[i][j]+=b[i][j];
+        }
+    }
+}
+
+void truHaiMaTranCungCap(MaTran a, MaTran b, int n){
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            a[i][j]-=b[i][j];
+        }
+    }
+}
+
+void nhanMaTranVoiMotSo(MaTran a, int n, int soNhan){
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            a[i][j]*=soNhan;
+        }
+    }
+}
+
+void nhanHaiMaTranCungCap(MaTran a, MaTran b, int n){
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            a[i][j]*=b[i][j];
+        }
+    }
+}
+
+
